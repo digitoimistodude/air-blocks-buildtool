3d9b2c6b11181a4d7bd503bdc32c9813f68e4bf7
raikasdev <roni.aikas@gmail.com>
2024-02-21 21:13:55 +0200

    Fix framework when editing in editor

--- php/framework.php
+++ php/framework.php
@@ -1,6 +1,8 @@
 <?php
-/** WP Block Builder "framework" */
-function register_attribute( $key, $name, $type, $default_value ) {}
+/**
+ * WP Block Builder "framework"
+ * @package air-blocks
+ */
 
 global $get_attribute_local_scope;
 $get_attribute_local_scope = function ( $key ) use ( $attributes ) {
@@ -10,21 +12,32 @@ $get_attribute_local_scope = function ( $key ) use ( $attributes ) {
   return $attributes[ $key ];
 };
 
-function attr( $key ) {
-  global $get_attribute_local_scope;
-  return $get_attribute_local_scope( $key );
+if ( ! function_exists( 'register_attribute' ) ) {
+  function register_attribute( $key, $name, $type, $default_value ) {}
 }
+
+if ( ! function_exists( 'attr' ) ) {
+  function attr( $key ) {
+    global $get_attribute_local_scope;
+    return $get_attribute_local_scope( $key );
+  }
+}
+
 // End WP Block Builder "framework"
 
 ob_start();
 require_once 'block.php';
 $html = ob_get_clean();
 
-// Replace innerblocks
-$html = preg_replace( '(<InnerBlocks.*\/{0,1}>)', $content, $html );
+$is_editor = defined( 'REST_REQUEST' ) && true === REST_REQUEST && 'edit' === filter_input( INPUT_GET, 'context', FILTER_SANITIZE_STRING );
 
-// Remove attributes
-$html = preg_replace( '( wp-[a-z\-]*=".*")', '', $html );
+if ( ! $is_editor ) {
+  // Replace innerblocks
+  $html = preg_replace( '(<InnerBlocks.*\/{0,1}>)', $content, $html );
+
+  // Remove attributes
+  $html = preg_replace( '( wp-[a-z\-]*=".*")', '', $html );
+}
 
 // We're just parsing content coming from blocks, so no escaping is necessary.
 echo $html; // phpcs:ignore
